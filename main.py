ca784eb672a6775e2030a8db16394d63406d341b
Sayem Raza <razasayem61@gmail.com>
2026-02-25 22:57:09 +0500

    Adding null validation and secondary greeting page routing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Request, Form, HTTPException
+from fastapi.responses import FileResponse, RedirectResponse
 
 
 app = FastAPI()
@@ -10,9 +10,12 @@ async def load_home(request: Request):
 
 @app.post("/submit-item")
 async def handle_form(my_name:str = Form(...),my_age:str = Form(...)):
-    print(f"The user typed: {my_name}")
-    print(f"The user's age is {my_age}")
-    return f"The message was successfully recieved: {my_name,my_age}"
+    if my_name.strip() and my_age.strip():
+        print(f"The user typed: {my_name}")
+        print(f"The user's age is {my_age}")
+        return RedirectResponse(url=f"/secondary_page?name={my_name}&age={my_age}", status_code=303)
+    else:
+        return HTTPException(status_code=400, detail="Invalid input: Both name and age are required.")
 
 @app.get("/secondary_page")
 async def load_secondary():
