283a042a976db729c38238775c3d9dd289c5afb6
Sayem Raza <razasayem61@gmail.com>
2026-02-27 23:12:48 +0500

    Installed Jinja2

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, Request, Form, HTTPException
-from fastapi.responses import FileResponse, RedirectResponse
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
 async def load_home(request: Request):
-    return FileResponse("homepage.html")
+    return templates.TemplateResponse("homepage.html", {"request": request})
 
 @app.post("/submit-item")
 async def handle_form(my_name:str = Form(...),my_age:str = Form(...)):
@@ -15,8 +18,9 @@ async def handle_form(my_name:str = Form(...),my_age:str = Form(...)):
         print(f"The user's age is {my_age}")
         return RedirectResponse(url=f"/secondary_page?name={my_name}&age={my_age}", status_code=303)
     else:
-        return HTTPException(status_code=400, detail="Invalid input: Both name and age are required.")
+        raise HTTPException(status_code=400, detail="Invalid input: Both name and age are required.")
 
 @app.get("/secondary_page")
-async def load_secondary():
-    return FileResponse("secondary.html")
+async def load_secondary(request: Request, name: str, age: str):
+    return templates.TemplateResponse("secondary.html",{"request": request, "name": name, "age": age})
+
